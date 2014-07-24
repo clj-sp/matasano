cf1009b68fe4fb89705712b8b465a25cbd223afe
Konrad Scorciapino <konr@konr.mobi>
2014-07-24 17:43:34 -0300

    midje and dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :plugins      [[lein-midje "3.1.3"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.taoensso/nippy "2.6.3"]
+                 [midje "1.6.3" :exclusions [org.clojure/core]]
+                 [org.clojure/data.codec "0.1.0"]
+                 [commons-codec/commons-codec "1.9"]])
