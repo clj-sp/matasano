8493a5ad3e5348b57f98eba2cd0c945766967f63
Nicolas Berger <nicoberger@gmail.com>
2014-09-05 18:27:01 -0300

    Fix best-score test: Pass bytes, not hex

--- test/matasano/core_test.clj
+++ test/matasano/core_test.clj
@@ -89,7 +89,7 @@
            "686974207468652062756c6c277320657965"))
 
 (expect "Cooking MC's like a pound of bacon"
-  (best-score "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"))
+  (best-score (hex->bytes "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")))
 
 (expect "Now that the party is jumping\n"
   (best-score-file "resources/challenge04"))
